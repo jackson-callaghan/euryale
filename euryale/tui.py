33d73626254326fbe1c96f8ee5e12817dcc3fbd2
token-diabetic <token-diabetic@users.noreply.github.com>
2020-01-20 18:28:50 -0500

    begin skill boxes

--- euryale/tui.py
+++ euryale/tui.py
@@ -11,9 +11,11 @@ from core import Character, utilities
 import gem.static as gs
 import os
 import string
+import math
 
 
 def main():
+
     termsize = get_terminal_size(fallback=(120, 29))
     size = (termsize[1] - 2, termsize[0])
 
@@ -66,6 +68,84 @@ def main():
         xsalign="aleft"
     )
 
+    skill_containers = []
+    skill_names = []
+    skill_text = []
+    skill_container_size = (
+        max(len(ab) for sk, ab in c.ability_map.items()) + 5,
+        max(max([len(a) for a in ab]) for sk, ab in c.ability_map.items()) + 4
+    )
+
+    row = 0
+    rowsize = 0
+    y = 0
+    n = 0
+
+    for i, skill in enumerate(c.ability_map.keys()):
+        if i == 0:
+            skill_containers.append(g.makedbox(
+                pos=(0, 0),
+                name=skill,
+                size=skill_container_size,
+                defaultpoints=True,
+                ytarget=details,
+                ytalign="bottom",
+                ysalign="below",
+                xtarget=details,
+                xtalign="left",
+                xsalign="aleft"
+            ))
+        elif y != row:
+            skill_containers.append(g.makedbox(
+                pos=(0, 0),
+                name=skill,
+                size=skill_container_size,
+                defaultpoints=True,
+                ytarget=skill_containers[i - n],
+                ytalign="bottom",
+                ysalign="below",
+                xtarget=skill_containers[i - n],
+                xtalign="left",
+                xsalign="aleft"
+            ))
+            row = y
+            rowsize = 0
+        else:
+            skill_containers.append(g.makedbox(
+                pos=(0, 0),
+                name=skill,
+                size=skill_container_size,
+                defaultpoints=True,
+                ytarget=skill_containers[i - 1],
+                ytalign="top",
+                ysalign="top",
+                xtarget=skill_containers[i - 1],
+                xtalign="oright",
+                xsalign="aleft"
+            ))
+        rowsize += skill_container_size[1]
+        if rowsize + skill_container_size[1] >= size[1]:
+            y += 1
+        n += 1
+
+    for sk in skill_containers:
+        skill_names.append(g.maketbox(
+            name="{} title".format(sk.name),
+            pos=(0, 0),
+            size=(1, 3),
+            text=sk.name[0:3].upper(),
+            bg="white",
+            fg="black",
+            ytarget=sk,
+            ytalign="top",
+            ysalign="center",
+            xtarget=sk,
+            xtalign="center",
+            xsalign="center"
+        ))
+        # ab_info  = " \n  \n".format()
+        # for ab in
+
     g.composite()
 
     while True:
@@ -73,9 +153,14 @@ def main():
         # check if terminal is resized and resize everything
         ntermsize = get_terminal_size(fallback=(120, 29))
         if ntermsize != termsize:
+
             termsize = ntermsize
             size = (termsize[1] - 2, termsize[0])
+
+            # resize compositor
             g.resize(size)  # don't make this one smaller than anything else
+
+            # resize details
             details.resize((5, size[1]), True, True)
             d_line1.resize((1, size[1] - 2))
             d_line2.resize((1, size[1] - 2))
@@ -83,6 +168,41 @@ def main():
                          len(c.name) if
                          len(c.name) <= size[1] - 2 else size[1] - 2
                          ))
+
+            row = 0
+            rowsize = 0
+            y = 0
+            n = 0
+
+            # resize skill boxes
+            for i, box in enumerate(skill_containers):
+                if i == 0:
+                    box.ytarget = details
+                    box.ytalign = "bottom"
+                    box.ysalign = "below"
+                    box.xtarget = details
+                    box.xtalign = "left"
+                    box.xsalign = "aleft"
+                elif y != row:
+                    box.ytarget = skill_containers[i - n]
+                    box.ytalign = "bottom"
+                    box.ysalign = "below"
+                    box.xtarget = skill_containers[i - n]
+                    box.xtalign = "left"
+                    box.xsalign = "aleft"
+                    row = y
+                    rowsize = 0
+                else:
+                    box.ytarget = skill_containers[i - 1]
+                    box.ytalign = "top"
+                    box.ysalign = "top"
+                    box.xtarget = skill_containers[i - 1]
+                    box.xtalign = "oright"
+                    box.xsalign = "aleft"
+                rowsize += skill_container_size[1]
+                if rowsize + skill_container_size[1] >= size[1]:
+                    y += 1
+                n += 1
 
         if c.name != name.text:
             name.resize((1,
