328efe80155fd89aa822e2737d06b6493fe23683
token-diabetic <43009413+token-diabetic@users.noreply.github.com>
2020-03-18 19:14:54 -0400

    begin modify method

--- euryale/core/registry.py
+++ euryale/core/registry.py
@@ -37,14 +37,56 @@ class RegItem:
         self.modlist = data.get("modlist")
         self.modtype = data.get("modtype")
         self.mod = data.get("mod")
+        self.parser = DiceParser()
 
     # TODO self handling method given number
-    def handle(n):
-        if self.activation == "passive":
+    def modify(self, n):
 
-            # TODO implement registry
+        # TODO add checking for currently active
+        # TODO change this to work for non-printing interfaces
+        if self.used is not None and self.charges is not None:
+            if self.used >= self.charges:
+                print("out of charges!")
+                return False
+                # TODO add attribute for word to use (charges/used/etc)
+                # TODO figure out what to return
+
+        # activation options
+        if self.activation == "passive":  # modifier happens passively
+            None  # not currently used for anything
+        elif self.activation == "active":  # mod never activates on its own
+            None  # not currently used for anything
+        elif self.activation == "reactive":  # mod is passive, but optional
+            print("")  # TODO print reminder and ask to use or not
+            # TODO needs some sort of callback function for this
+            # actually consider how you want to handle this so it works
+            # for multiple interfaces and isn't a pain to deal with
+        elif self.activation == "reminder":  # print desc for whatever reason
+            print(self.description)  # for now just this
+            # TODO change this to handle non-printing interfaces
+        else:
+            raise ValueError("unexpected activation value")
+
+        # charges and duration
+        if self.used is not None and self.charges is not None:
+            self.used += self.chperuse
+        if self.duration is not None:
+            self.active = True
+
+        # return modification
+        if self.modtype == "delta":
+            return n + self.parser.parse(self.mod).roll()
+        elif self.modtype == "mult":
+            return n * self.parser.parse(self.mod).roll()
+        elif self.modtype == "div":
+            return n / self.parser.parse(self.mod).roll()
+        elif self.modtype == "min":
+            return self.mod if n < self.mod else n
+        elif self.modtype == "max":
+            return self.mod if n > self.mod else n
 
 
+# TODO implement registry
 class Registry:
     """Registry.
 
